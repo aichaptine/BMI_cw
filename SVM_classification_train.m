9523ba9384552d1ef0452c77038d7a2834802e4b
aichaptine <44064408+aichaptine@users.noreply.github.com>
2019-03-09 15:41:06 +0000

    Update SVM_classification_train.m

--- SVM_classification_train.m
+++ SVM_classification_train.m
@@ -1,17 +1,20 @@
-function [model_1_2, model_1_3, model_1_4, model1_5] = SVM_multiclass_Train(data)
+function [models] = SVM_multiclass_Train(data)
 
 sigma=0.1;
 C=1.0;
+models = zeros(1,28);
 
 [X,y,id] = preprocess_planning_data(data);
 
 %Pairwise method
 %loop to go through all angles
-for i=1:7
-  for j= i+1:8
-  xi = X(:,find(y==i));
-  xj = X(:,find(y==j));
-  model = SVM_train(xi, xj,sigma,c);
+for v=1:length(models
+  for i=1:7
+    for j= i+1:8
+    xi = X(:,find(y==i));
+    xj = X(:,find(y==j));
+    models(v) = SVM_train(xi, xj,sigma,c);
+    end
   end
 end
   
