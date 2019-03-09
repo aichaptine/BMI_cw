6f67e53564c422eacaf7242f2ef4d9085904af4e
auxtinemicalet <aem15@ic.ac.uk>
2019-03-09 15:55:53 +0000

    Update SVM_train.m

--- SVM_train.m
+++ SVM_train.m
@@ -1,8 +1,10 @@
 function model = SVM_train(xi, xj, sigma, C)
 
-X = [xi xj];
-y = [ones(1,size(xi,2)) zeros(1,size(xj,2))];
+% binary svm classification
 
-model = svmTrain(X', y',C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+X = [xi xj]; %keeps only the training data relevant to this angle pair
+y = [ones(1,size(xi,2)) zeros(1,size(xj,2))];  %processes the train data's labels: transforming to 1s for angle i and 0s for angle j
+
+model = svmTrain(X', y',C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
 
 end
