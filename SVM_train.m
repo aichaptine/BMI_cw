710fd624b162cbfe29489bbdb28782482aca6e6b
aichaptine <44064408+aichaptine@users.noreply.github.com>
2019-03-09 15:44:03 +0000

    Update SVM_train.m

--- SVM_train.m
+++ SVM_train.m
@@ -1,7 +1,7 @@
 function model = SVM_train(xi, xj, sigma, C)
 
 X = [xi xj];
-y = [ones(1,length(xi)) zeros(1,length(xj))];
+y = [ones(1,size(xi,2)) zeros(1,size(xj,2))];
 
 model = svmTrain(X', y',C, @(x1, x2) gaussianKernel(x1, x2, sigma));
 
