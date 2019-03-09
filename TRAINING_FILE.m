ce59b27cf926e772df060705d45394cf019e0303
aichaptine <44064408+aichaptine@users.noreply.github.com>
2019-03-09 16:35:48 +0000

    Update TRAINING_FILE.m

--- TRAINING_FILE.m
+++ TRAINING_FILE.m
@@ -4,15 +4,12 @@ load('monkeydata_training.mat');
 
 [X,y,id] = preprocess_planning_data(trial);
 X_train = X(:, 1:640);
-y_train = X(:, 1:640);
+y_train = y(:, 1:640);
 X_test =  X(:, 641:end);
-y_test =  X(:, 641:end);
+y_test =  y(:, 641:end);
 out = zeros(28,length(X_test));
 models = SVM_multiclass_train(X_train,y_train);
 
 for i=1:length(models)
     out(i,:) = svmPredict(models(i), X_test');
 end
-
-%svmPredict(model, X)
-%loop through all the models
