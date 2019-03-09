3349fe593f9d7ecc06c879fd74567e33c9dd7d1d
aichaptine <44064408+aichaptine@users.noreply.github.com>
2019-03-09 16:31:26 +0000

    Create TRAINING_FILE.m

---
+++ TRAINING_FILE.m
@@ -0,0 +1,18 @@
+load('monkeydata_training.mat');
+
+%% Classifying different reaching angles using the 300ms before movement.
+
+[X,y,id] = preprocess_planning_data(trial);
+X_train = X(:, 1:640);
+y_train = X(:, 1:640);
+X_test =  X(:, 641:end);
+y_test =  X(:, 641:end);
+out = zeros(28,length(X_test));
+models = SVM_multiclass_train(X_train,y_train);
+
+for i=1:length(models)
+    out(i,:) = svmPredict(models(i), X_test');
+end
+
+%svmPredict(model, X)
+%loop through all the models
