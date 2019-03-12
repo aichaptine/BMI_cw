25bcea5453ee2c863e9826bffe0f29af042af4e5
aichaptine <44064408+aichaptine@users.noreply.github.com>
2019-03-12 15:41:50 +0000

    Update count_occurrences.m

--- count_occurrences.m
+++ count_occurrences.m
@@ -1,4 +1,9 @@
 function predicted_angles = count_occurrences(out)
+
+%The input of this function is a matrix of the predicted angles of the test data having gone through for each model. 
+%At each trial, the most occurrent angle is the predicted angle.
+%The function outputs a vector of predicted angles for each trial.
+
     y = zeros(1,8);
     predicted_angles = zeros(1,size(out,2));
     
