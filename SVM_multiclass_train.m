3ef600333c49bfd9d8a0aeaf092a8064ab152bb9
aichaptine <44064408+aichaptine@users.noreply.github.com>
2019-03-12 15:22:27 +0000

    Update SVM_multiclass_train.m

--- SVM_multiclass_train.m
+++ SVM_multiclass_train.m
@@ -1,22 +1,19 @@
-function [models] = SVM_multiclass_train(X_train,y_train)
+
+function [models] = SVM_multiclass_train(X_train,y_train, sigma, c)
 
 % we aim to determine the reaching angle using the first 300ms. 
 % the input is the training data and output is 28 models. e.g. model for angle 1 vs angle 2
 % We use a one vs one multiclass SVM classification
 
-
-sigma=0.1; C=1.0;  % variables for the svm classification
-
 %Pairwise method
 %loop to go through all angles
 m = 0;
-for i=1:7
+
+  for i=1:7
     for j= i+1:8
     m = m+1;
-    xi = X_train(:,find(y==i));  %finds training data relevant to this angle pair
-    xj = y_train(:,find(y==j));
-    models(m) = SVM_train(xi, xj,sigma,c); %trains the data for this angle pair
+    xi = X_train(:,find(y_train==i));
+    xj = X_train(:,find(y_train==j));
+    models(m) = SVM_train(xi, xj,sigma,c);
     end
   end
-
-  
