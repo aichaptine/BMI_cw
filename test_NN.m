11dcc9a0141bb9a8305dd1c58e9bba9af0eca0a2
jowarren97 <jw8415@ic.ac.uk>
2019-03-12 16:19:22 +0000

    Train/test NN model for single angle
    Plot trajectories

--- test_NN.m
+++ test_NN.m
@@ -3,7 +3,7 @@ load('monkeydata_training.mat')
 
 %Take only one reaching angle and train model
 %N.B. only took first 80 trials for training; remainder are for testing
-[X_move, y_move, id_move] = preprocess_movement_data(trial(1:80,1), 3, 20);
+[X_move, y_move, id_move] = preprocess_movement_data(trial(1:80,6), 5, 20);
 
 %[X_move, y_move, id_move] = preprocess_movement_data(trial, 3, 20);
 [X_plan, y_plan, id_plan] = preprocess_planning_data(trial);
@@ -39,55 +39,73 @@ X_train((sx==0), :) = [];
 
 
 %% TRAIN NN, 100 HIDDEN UNITS
-net = feedforwardnet(100);
+net = feedforwardnet(200);
 [net, tr] = trainscg(net, X_train, y_train)
 
 %% TEST ON SPECIFIC TRIAL (I.E. NOT IN TRAINING SET)
 %test on trial (M,1)
-X_t = [];
-y_t = [];
+
 L=20;
-B=3;
-M=88;
-movement_spikes = trial(M,1).spikes(:,300:end); %300 or 301?
-movement_handPos = trial(M,1).handPos(:,300:end);
-T = size(movement_spikes, 2); %gives length of cut sample
+B=5;
+for M = 81:100
+    X_t = [];
+    y_t = [];
+    movement_spikes = trial(M,6).spikes(:,260:end); %300 or 301?
+    movement_handPos = trial(M,6).handPos(:,260:end);
+    T = size(movement_spikes, 2); %gives length of cut sample
 
-for k = B*L:L:T
-    spike_sample = movement_spikes(:, k+1-B*L : k);
+    for k = B*L:L:T
+        spike_sample = movement_spikes(:, k+1-B*L : k);
 
-    %calculate features (firing rates) for all 98 neurons for each bin
-    feature_sample = splice_data(spike_sample, B);
+        %calculate features (firing rates) for all 98 neurons for each bin
+        feature_sample = splice_data(spike_sample, B);
 
-    %calculate change in x,y position over 20ms (L) interval
-    delta_handPos_sample = movement_handPos(1:2, k) - movement_handPos(1:2, k+1-L); 
+        %calculate change in x,y position over 20ms (L) interval
+        delta_handPos_sample = movement_handPos(1:2, k) - movement_handPos(1:2, k+1-L); 
 
-    %append above to the output matrices
-    feature_sample = (feature_sample - mx)./sx;
-    X_t = [X_t, feature_sample];
-    y_t = [y_t, delta_handPos_sample];
-end 
+        %append above to the output matrices
+        feature_sample = (feature_sample - mx)./sx;
+        X_t = [X_t, feature_sample];
+        y_t = [y_t, delta_handPos_sample];
+    end 
 
-%NEED TO MAKE SURE WE REMOVE ROWS OF TEST DATA THAT WE REMOVED FOR TESTING
-X_t((sx==0),:) = [];
+    %NEED TO MAKE SURE WE REMOVE ROWS OF TEST DATA THAT WE REMOVED FOR TESTING
+    X_t((sx==0),:) = [];
 
-y_pred = net(X_t);
-y_pred = y_pred + my; %undo zero-centre output
+    y_pred = net(X_t);
+    y_pred = y_pred + my; %undo zero-centre output
 
-figure()
-hold on
-plot(y_pred(1,:))
-plot(y_t(1,:))
-hold off
+%     figure()
+%     subplot(1,2,1)
+%     hold on
+%     plot(y_pred(1,:))
+%     plot(y_t(1,:))
+%     hold off
+%     xlabel('Bin number')
+%     ylabel('Predicted x movement')
+%     subplot(1,2,2)
+%     hold on
+%     plot(y_pred(2,:))
+%     plot(y_t(2,:))
+%     hold off
+%     legend('Predicted', 'True')
+%     xlabel('Bin number')
+%     ylabel('Predicted y movement')
 
-pos_pred = cumsum(y_pred,2);
-pos_true = cumsum(y_t,2);
+    pos_pred = cumsum(y_pred,2);
+    pos_true = cumsum(y_t,2);
 
-figure()
-[theta,rho] = cart2pol(pos_pred(1,:), pos_pred(2,:));
-polarplot(theta,rho)
-hold on
-[theta,rho] = cart2pol(pos_true(1,:), pos_true(2,:));
-polarplot(theta,rho)
-legend('Predicted', 'True')
-hold off
+    %figure()
+    [theta,rho] = cart2pol(pos_pred(1,:), pos_pred(2,:));
+    polarplot(theta,rho,'r')
+    rlim([0 120])
+    hold on
+    polarplot(theta(end),rho(end),'*r')
+    [theta,rho] = cart2pol(pos_true(1,:), pos_true(2,:));
+    polarplot(theta,rho,'b')
+    polarplot(theta(end),rho(end),'*b')
+    legend('Predicted Trajectory', '', 'True Trajectory', '')
+    hold off
+    pause(1)
+    %close all;
+end
