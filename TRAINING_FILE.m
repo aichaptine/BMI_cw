ac895c1a51577aa8fab0d4c4e9b3206b82903dd6
aichaptine <44064408+aichaptine@users.noreply.github.com>
2019-03-12 15:23:15 +0000

    Update TRAINING_FILE.m

--- TRAINING_FILE.m
+++ TRAINING_FILE.m
@@ -1,15 +1,27 @@
+clear all;
 load('monkeydata_training.mat');
 
 %% Classifying different reaching angles using the 300ms before movement.
 
-[X,y,id] = preprocess_planning_data(trial);
-X_train = X(:, 1:640);
+sigma=500;
+c=1.0; %Cross-validation to find best values for these may be needed
+
+[X,y,id] = preprocess_planning_data(trial);  %preprocess the data, returns: firing rate for first 300ms in 'X', 
+                                            %Takes reaching angle for trial in 'y'  and Takes trial id in 'id'
+%[X,y,id] = shuffle_data(X, y, id);
+X_train = X(:, 1:640); 
 y_train = y(:, 1:640);
 X_test =  X(:, 641:end);
 y_test =  y(:, 641:end);
-out = zeros(28,length(X_test));
-models = SVM_multiclass_train(X_train,y_train);
+out = zeros(28,size(X_test, 2));
+models = SVM_multiclass_train(X_train,y_train,sigma,c);
 
 for i=1:length(models)
-    out(i,:) = svmPredict(models(i), X_test);
+    out(i,:) = svmPredict(models(i), X_test');
 end
+
+predictions = convert_to_RA(out);
+predicted_angle = count_occurrences(predictions); %Counts the number of occurrence of each 
+dist = predicted_angle-y_test;
+error = 1-(sum(dist==0)/length(dist));
+accuracy = 1-error;
